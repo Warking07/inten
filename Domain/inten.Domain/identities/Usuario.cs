3fd46a3b12f46c529dfe50f27bf8629b00c479f5
Gean <Gean@DESKTOP-777B9HN>
2024-02-08 00:40:48 -0400

    Actualizacion.

---
+++ Domain/inten.Domain/identities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace inten.Domain.identities
+{
+    internal class Usuario
+    {
+        public int idUsuario { get; set; }
+        public string? nombreApellidos { get; set; }
+        public string? correos { get; set; }
+        public string? clave { get; set; }
+        public bool esActivo { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+
+
+    }
+}
--- Domain/inten.Domain/obj/Debug/net6.0/inten.Domain.AssemblyInfo.cs
+++ Domain/inten.Domain/obj/Debug/net6.0/inten.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inten.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540ee224426e7eea0661acfdb79c29422112f767")]
 [assembly: System.Reflection.AssemblyProductAttribute("inten.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inten.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
